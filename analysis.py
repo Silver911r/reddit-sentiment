4674f5eae4a9f0786888383bebff49842cb4b32b
bigminion <bigminion@iMac.local>
2021-06-01 09:18:38 -0400

    fixed naming issues  with csv and save fig to folder when using analysys.py

--- analysis.py
+++ analysis.py
@@ -5,14 +5,16 @@ import seaborn as sns
 import praw
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+import ssl
 import config
 
 
 reddit = praw.Reddit(
     client_id=config.CLIENT_ID,
     client_secret=config.CLIENT_SECRET,
-    user_agent=config.USER_AGENT
+    user_agent=config.USER_AGENT,
 )
+
 
 def get_sentiment_r(reddit_group):
     # hot new rising top
@@ -23,42 +25,58 @@ def get_sentiment_r(reddit_group):
 
     headlines_df = pd.DataFrame(headlines)
 
-    headlines_df.to_csv(reddit_group + '.csv', header=False, encoding='utf-8', index=False)
+    headlines_df.to_csv(
+        reddit_group + ".csv", header=False, encoding="utf-8", index=False
+    )
 
-    nltk.download('vader_lexicon')
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+
+    nltk.download("vader_lexicon")
 
     sia = SIA()
     results = []
 
     for line in headlines:
-        pol_score = sia.polarity_scores(line) # -> dict
-        pol_score['headline'] = line
+        pol_score = sia.polarity_scores(line)  # -> dict
+        pol_score["headline"] = line
         results.append(pol_score)
-        
-    print(results[:5], width=100)
+
+    print(results[:5])
 
     df = pd.DataFrame.from_records(results)
 
-    df['label'] = 0
-    df.loc[df['compound'] > 0.2, 'label'] = 1
-    df.loc[df['compound'] < -0.2, 'label'] = -1
+    df["label"] = 0
+    df.loc[df["compound"] > 0.2, "label"] = 1
+    df.loc[df["compound"] < -0.2, "label"] = -1
 
-    df2 = df[['headline','label']]
+    df2 = df[["headline", "label"]]
 
-    df2.to_csv(reddit_group+'_headlines_labels.csv', encoding='utf-8', index=False)
+    df2.to_csv(
+        "csv_files_headlines/" + reddit_group + "_headlines_labels.csv",
+        encoding="utf-8",
+        index=False,
+    )
 
     df.label.value_counts()
 
     df.label.value_counts(normalize=True) * 100
 
-    print("Positive "+ reddit_group +" headlines:\n")
-    print(list(df[df['label'] == 1 ].headline)[:5], width=200)
+    print("Positive " + reddit_group + " headlines:\n")
+    print(list(df[df["label"] == 1].headline)[:5])
 
     print("\nNegative " + reddit_group + " headlines:\n")
-    print(list(df[df['label'] == -1].headline)[:5], width=200)
+    print(list(df[df["label"] == -1].headline)[:5])
 
     fig, ax = plt.subplots(figsize=(8, 8))
     counts = df.label.value_counts(normalize=True) * 100
     sns.barplot(x=counts.index, y=counts, ax=ax)
 
-get_sentiment_r('investing')
+    fig.savefig("sentiment_charts/" + reddit_group + ".png")
+
+
+get_sentiment_r("stocks")
