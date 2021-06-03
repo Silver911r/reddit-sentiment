3f3d586121f8259b9c0d9a70e17456b34ac7b300
bigminion <bigminion@iMac.local>
2021-06-03 09:35:20 -0400

    corrected subreddit name in file

--- sentiment_for_subreddits.py
+++ sentiment_for_subreddits.py
@@ -3,7 +3,7 @@ import analysis
 if __name__ == "__main__":
     analysis.get_sentiment_r("stocks")
     analysis.get_sentiment_r("finance")
-    analysis.get_sentiment_r("option")
+    analysis.get_sentiment_r("options")
     analysis.get_sentiment_r("wallstreetbets")
     analysis.get_sentiment_r("stockmarket")
     analysis.get_sentiment_r("investing")
